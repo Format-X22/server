d36d7a96d7fa7d4390f0ff329df07ae39326baa8
Oleg <oleg.pav.m@gmail.com>
2019-04-03 15:29:38 +0300

    Connector api.

--- src/services/Connector.js
+++ src/services/Connector.js
@@ -1,12 +1,13 @@
-const merge = require('deepmerge'); // TODO Remove from package
 const core = require('gls-core-service');
 const BasicConnector = core.services.Connector;
+const env = require('../data/env');
 const Admin = require('../controllers/Admin');
 const Balance = require('../controllers/Balance');
 const Exchange = require('../controllers/Exchange');
 const History = require('../controllers/History');
-const Auth = require('../utils/Auth'); // TODO -
+const Auth = require('../utils/Auth');
 
+// TODO Core validation types
 const NUMBER_ID_VALIDATION = {
     type: 'number',
     minValue: 0,
@@ -19,29 +20,7 @@ const ACCOUNT_VALIDATION = {
     type: 'string',
     pattern: '^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$',
 };
-const BASIC_VALIDATION = {
-    type: 'object',
-    additionalProperties: false,
-    required: ['service'],
-    properties: {
-        service: {
-            type: 'object',
-            additionalProperties: false,
-            required: ['version', 'account', 'sign', 'identityKey', 'timestamp'],
-            properties: {
-                version: NUMBER_ID_VALIDATION,
-                account: ACCOUNT_VALIDATION,
-                identityKey: UID_VALIDATION,
-                sign: { type: 'string', maxLength: 1024 },
-                timestamp: { type: 'number', minValue: 1553000000000 },
-            },
-        },
-    },
-};
 
-// TODO Check auth + identity
-// TODO Preprocessors + basic preprocessors with off flag
-// TODO Basic validation with defaults from core
 class Connector extends BasicConnector {
     constructor({ identityService }) {
         super();
@@ -53,112 +32,206 @@ class Connector extends BasicConnector {
         this._exchange = new Exchange(linking);
         this._history = new History(linking);
 
-        this._identity = identityService; // TODO -
+        this._identity = identityService;
+        this._auth = new Auth(); // TODO Keys
     }
 
     async start() {
         await super.start({
             serverRoutes: {
                 'balance.get': {
+                    inherits: ['service'],
                     handler: this._balance.get,
                     scope: this._balance,
-                    validation: BASIC_VALIDATION,
                 },
                 'balance.getFor': {
+                    inherits: ['service'],
                     handler: this._balance.getFor,
                     scope: this._balance,
-                    validation: merge(BASIC_VALIDATION, {
-                        required: ['targetAccount'],
+                    validation: {
+                        required: ['accountId'],
                         properties: {
-                            targetAccount: ACCOUNT_VALIDATION,
+                            accountId: ACCOUNT_VALIDATION,
                         },
-                    }),
+                    },
                 },
                 'balance.send': {
+                    inherits: ['service'],
                     handler: this._balance.send,
                     scope: this._balance,
-                    validation: merge(BASIC_VALIDATION, {
-                        required: ['targetAccount'],
+                    validation: {
+                        required: ['accountId'],
                         properties: {
-                            targetAccount: ACCOUNT_VALIDATION,
+                            accountId: ACCOUNT_VALIDATION,
                             exchangeId: UID_VALIDATION,
-                            message: { type: 'string', maxLength: 2048 },
+                            message: {
+                                type: 'string',
+                                maxLength: 2048,
+                            },
                         },
-                    }),
+                    },
                 },
                 'exchange.request': {
+                    inherits: ['service'],
                     handler: this._exchange.request,
                     scope: this._exchange,
-                    validation: merge(BASIC_VALIDATION, {
+                    validation: {
                         // TODO -
-                    }),
+                    },
                 },
                 'exchange.approve': {
+                    inherits: ['service'],
                     handler: this._exchange.approve,
                     scope: this._exchange,
-                    validation: merge(BASIC_VALIDATION, {
+                    validation: {
                         // TODO -
-                    }),
+                    },
                 },
                 'history.get': {
+                    inherits: ['service', 'sequence'],
                     handler: this._history.get,
                     scope: this._history,
-                    validation: merge(BASIC_VALIDATION, {
-                        properties: {
-                            sequenceKey: UID_VALIDATION,
-                        },
-                    }),
                 },
                 'history.getFor': {
+                    inherits: ['service', 'sequence'],
                     handler: this._history.getFor,
                     scope: this._history,
-                    validation: merge(BASIC_VALIDATION, {
-                        required: ['targetAccount'],
+                    validation: {
+                        required: ['accountId'],
                         properties: {
-                            sequenceKey: UID_VALIDATION,
-                            targetAccount: ACCOUNT_VALIDATION,
+                            accountId: ACCOUNT_VALIDATION,
                         },
-                    }),
+                    },
                 },
                 'history.getForAll': {
+                    inherits: ['service', 'sequence'],
                     handler: this._history.getForAll,
                     scope: this._history,
-                    validation: merge(BASIC_VALIDATION, {
-                        properties: {
-                            sequenceKey: UID_VALIDATION,
-                        },
-                    }),
                 },
                 'history.getBy': {
+                    inherits: ['service'],
                     handler: this._history.getBy,
                     scope: this._history,
-                    validation: merge(BASIC_VALIDATION, {
+                    validation: {
                         required: ['transactionId'],
                         properties: {
                             transactionId: NUMBER_ID_VALIDATION,
                         },
-                    }),
+                    },
+                },
+                'admin.incrementBalance': {
+                    inherits: ['service', 'balanceManipulation'],
+                    handler: this._admin.incrementBalance,
+                    scope: this._admin,
+                },
+                'admin.decrementBalance': {
+                    inherits: ['service', 'balanceManipulation'],
+                    handler: this._admin.decrementBalance,
+                    scope: this._admin,
                 },
                 'admin.openExchange': {
+                    inherits: ['service'],
                     handler: this._admin.openExchange,
                     scope: this._admin,
-                    validation: merge(BASIC_VALIDATION, {
+                    validation: {
                         // TODO -
-                    }),
+                    },
                 },
                 'admin.editExchange': {
+                    inherits: ['service'],
                     handler: this._admin.editExchange,
                     scope: this._admin,
-                    validation: merge(BASIC_VALIDATION, {
+                    validation: {
                         // TODO -
-                    }),
+                    },
                 },
                 'admin.closeExchange': {
+                    inherits: ['service'],
                     handler: this._admin.closeExchange,
                     scope: this._admin,
-                    validation: merge(BASIC_VALIDATION, {
+                    validation: {
                         // TODO -
-                    }),
+                    },
+                },
+            },
+            serverDefaults: {
+                parents: {
+                    service: {
+                        before: [
+                            {
+                                handler: this._auth.verify,
+                                scope: this._auth,
+                            },
+                            {
+                                handler: this._identity.verify,
+                                scope: this._identity,
+                            },
+                        ],
+                        validation: {
+                            required: ['service'],
+                            properties: {
+                                service: {
+                                    type: 'object',
+                                    additionalProperties: false,
+                                    required: [
+                                        'version',
+                                        'accountId',
+                                        'sign',
+                                        'identityKey',
+                                        'timestamp',
+                                    ],
+                                    properties: {
+                                        version: NUMBER_ID_VALIDATION,
+                                        accountId: ACCOUNT_VALIDATION,
+                                        identityKey: UID_VALIDATION,
+                                        sign: {
+                                            type: 'string',
+                                            maxLength: 1024,
+                                        },
+                                        timestamp: {
+                                            type: 'number',
+                                            minValue: 1553000000000,
+                                        },
+                                    },
+                                },
+                            },
+                        },
+                    },
+                    sequence: {
+                        validation: {
+                            properties: {
+                                sequenceKey: {
+                                    type: ['string', 'null'],
+                                    default: null,
+                                    maxLength: 256,
+                                },
+                                limit: {
+                                    type: 'number',
+                                    default: 10,
+                                    minValue: 1,
+                                    maxValue: env.DS_MAX_HISTORY_LIMIT,
+                                },
+                            },
+                        },
+                    },
+                    balanceManipulation: {
+                        validation: {
+                            required: ['accountId', 'assetTypeId', 'amount'],
+                            properties: {
+                                accountId: ACCOUNT_VALIDATION,
+                                assetTypeId: {
+                                    type: 'string',
+                                },
+                                assetUniqueId: {
+                                    type: 'string',
+                                    default: '',
+                                },
+                                amount: {
+                                    type: ['string', 'number'],
+                                },
+                            },
+                        },
+                    },
                 },
             },
         });
